0ba4283335c735d3823f4fed5b1e54f1617af47c
Willy <willyhome85@gmail.com>
2018-06-26 13:55:32 +0800

    Upload code

---
+++ homework1/101502040_張家豪_作業一-01/Perceptron/src/Main.java
@@ -0,0 +1,13 @@
+import java.awt.*;
+import java.util.ArrayList;
+
+import javax.swing.*;
+
+
+
+public class Main {
+	static ArrayList<double[]> data = new ArrayList();
+	public static void main(String[] args ){
+		GUI gui = new GUI();
+	}
+}
